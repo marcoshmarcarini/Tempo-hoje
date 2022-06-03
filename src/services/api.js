0696ce5f95e4d4cac6b8d9d29e9a730f844b1d7c
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2022-06-03 14:33:48 -0300

    Update
    Subindo toda a aplicação..

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+const KEY = 'c9d7fec365d645ffa5a131003220306'
+
+const fetchData = async (city) => {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`
+    const fetchResponse = await fetch(url)
+    const data = await fetchResponse.json()
+    return data
+}
+
+export default fetchData
